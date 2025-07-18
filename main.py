c16e9cdbfd024a009b3cd70ac263971131afac7a
Usman Nisar <un41960@gmail.com>
2025-07-18 15:47:27 +0500

    Added Rendering for data

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import markdown2 
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader(searchpath='./'))
+file = env.get_template('base.html')
 
 with open('test.md', 'r', encoding='utf-8') as markdown_file:
     text = markdown_file.read()
@@ -6,5 +10,9 @@ with open('test.md', 'r', encoding='utf-8') as markdown_file:
 html_code = markdown2.markdown(text)
 
 with open('output.html', 'w', encoding='utf-8') as html_file:
-    html_file.write(html_code)
+    html_file.write(
+        file.render(
+            content="This is the content"
+        )
+    )
 
